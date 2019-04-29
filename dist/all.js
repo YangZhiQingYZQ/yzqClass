dde049152e2d04cb862e9ef3db0b0eb9b297b901
yangzhiqing <493583130@qq.com>
2019-04-29 11:20:36 +0800

    更改正则文件，添加判断环境类

--- dist/all.js
+++ dist/all.js
@@ -1,25 +1,102 @@
+var HandlerBrowser = (function () {
+    //判断网页运行环境
+    var u = navigator.userAgent,
+        BrowseType = {
+            ie: 'Trident', //IE内核
+            webKit: 'AppleWebKit', //苹果、谷歌内核
+            presto: 'Presto', //opera内核
+            mobile: 'Mobile', //是否为移动终端
+            iPhone: 'iPhone', //是否为iPhone或者QQHD浏览器
+            iPad: 'iPad', //是否iPad
+            webApp: 'Safari', //是否web应该程序，没有头部与底部
+            weixin: 'MicroMessenger', //是否微信
+            qq: 'QQBrowser', //是否QQ浏览器
+            mobile_qq: 'MQQBrowser', //是否是手机QQ浏览器
+            uc: 'UCBrowser', //是否是uc浏览器
+            baidu: 'Baidu', //是否是百度浏览器
+            firefox: 'Firefox', //是否是火狐浏览器
+            lieBao: 'lieBao', //是否是猎豹浏览器
+            sogou: 'sogou', //是否是搜狗手机浏览器
+        }
+    return function () {
+        this.result = {}; //缓存的结果变量  
+        this.isBrowse = function (key) { //判断网页所处环境,并缓存结果
+            if (!this.result.hasOwnProperty(key) && BrowseType[key]) {
+                this.result[key] = u.indexOf(BrowseType[key]) > -1;
+            } else {
+                throw new Error("没有" + key + "这个判断属性");
+            }
+            return this;
+        }
+        this.addBrowseType = function (key, val) { //新增网页判断环境
+            BrowseType[key] = val;
+            return this;
+        };
+        this.addResult = function (callBack) { //新增网页判断结果，非常规判断
+            callBack.call(this, u);
+            return this;
+        }
+    }
+})();
 console.log("b")
 console.log("d")
 console.log("c")
-function HandlerReg() {
-	this.mobile = /^1[3456789]\d{9}$/; //手机号
-	this.pwd = /[\d\w]{6,20}/; //密码
-	this.code = /^\d{4}$/; //图片验证码&短信验证码均为4位数字
-	this.name = /^[\u4e00-\u9fa5]{2,4}$/; //名字（汉字）
-	this.msgCode = /^\d{6}$/; //手机验证码
-	this.idCard = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/; //身份证
-	this.email = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/; //邮箱
-	this.bankNo = /^([1-9]{1})(\d{15}|\d{18})$/; //银行卡
-	this.telPhone = /^\d{3,4}\-?\d{7,8}((\-?\d{6})|\d{0})$/; //住宅电话
-	this.monthSalary = /^[1-9]\d*$/; //月收入;
-}
-HandlerReg.prototype = {
-	isVerify : function(type,val){
-		return this[type].test(val);
+var HandlerReg = function () {
+	var resType = {
+		mobile: /^1[3456789]\d{9}$/, //手机号
+		pwd: /[\d\w]{6,20}/, //密码
+		code: /^\d{4}$/, //图片验证码&短信验证码均为4位数字
+		name: /^[\u4e00-\u9fa5]{2,4}$/, //名字（汉字）
+		msgCode: /^\d{6}$/, //手机验证码
+		idCard: /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/, //身份证
+		email: /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/, //邮箱
+		bankNo: /^([1-9]{1})(\d{15}|\d{18})$/, //银行卡
+		telPhone: /^\d{3,4}\-?\d{7,8}((\-?\d{6})|\d{0})$/, //住宅电话
+		monthSalary: /^[1-9]\d*$/, //月收入;
+		name: /^[\u4e00-\u9fa5]{2,20}$/i, //姓名
+		isNumber: /^[1-9][0-9]*$/i, //数字
+		email: /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/i, //邮箱
+		money: /^([1-9][\d]{0,7}|0)(\.[\d]{1,2})?$/i, //金钱
 	}
-}
-var reg = new HandlerReg().isVerify("code","112")
-console.log(reg);
+	var repformatting = {
+		// 银行卡号 每四位 一个空格
+		SpaceBankCode: function (val) {
+			return (val + "").replace(/\s/g, '').replace(/(.{4})/g, "$1 ")
+		},
+		//银行卡号 显示四三位和后三位
+		encryptBankCode: function (val) {
+			return (val + "").replace(/^(.{4}).*(.{3})$/, "$1 **** **** *$2")
+		},
+		//手机号码 显示前三位和后三位
+		phone:function(val){
+			return (val + "").replace(/^(.{3}).*(.{3})$/, "$1*****$2")
+		},
+		// 更改姓名显示，只显示最后一个
+		userName: function (val) {
+			var strArr = val.split(""),
+				len = strArr.length - 1,
+				i = 0;
+			for (; i < len; i++) {
+				strArr[i] = strArr[i].replace(strArr[i], "*");
+			}
+			return strArr.join("");
+		},
+	}
+	//验证方法
+	function isVerify(type, val) {
+		return resType[type].test(val);
+	}
+	//替换方法
+	function replaceType(type, val) {
+		return repformatting[type](val);
+	}
+	//暴露方法对象
+	return {
+		isVerify: isVerify,
+		replaceType: replaceType
+	}
+};
+console.log(HandlerReg().replaceType('encryptBankCode', '6228480078809886479'));
 function HandlerUrl() {
     this.getQuery = function () {
         var query = {},
--- src/js/regClass.js
+++ src/js/regClass.js
@@ -1,19 +1,56 @@
-function HandlerReg() {
-	this.mobile = /^1[3456789]\d{9}$/; //手机号
-	this.pwd = /[\d\w]{6,20}/; //密码
-	this.code = /^\d{4}$/; //图片验证码&短信验证码均为4位数字
-	this.name = /^[\u4e00-\u9fa5]{2,4}$/; //名字（汉字）
-	this.msgCode = /^\d{6}$/; //手机验证码
-	this.idCard = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/; //身份证
-	this.email = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/; //邮箱
-	this.bankNo = /^([1-9]{1})(\d{15}|\d{18})$/; //银行卡
-	this.telPhone = /^\d{3,4}\-?\d{7,8}((\-?\d{6})|\d{0})$/; //住宅电话
-	this.monthSalary = /^[1-9]\d*$/; //月收入;
-}
-HandlerReg.prototype = {
-	isVerify : function(type,val){
-		return this[type].test(val);
+var HandlerReg = function () {
+	var resType = {
+		mobile: /^1[3456789]\d{9}$/, //手机号
+		pwd: /[\d\w]{6,20}/, //密码
+		code: /^\d{4}$/, //图片验证码&短信验证码均为4位数字
+		name: /^[\u4e00-\u9fa5]{2,4}$/, //名字（汉字）
+		msgCode: /^\d{6}$/, //手机验证码
+		idCard: /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/, //身份证
+		email: /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/, //邮箱
+		bankNo: /^([1-9]{1})(\d{15}|\d{18})$/, //银行卡
+		telPhone: /^\d{3,4}\-?\d{7,8}((\-?\d{6})|\d{0})$/, //住宅电话
+		monthSalary: /^[1-9]\d*$/, //月收入;
+		name: /^[\u4e00-\u9fa5]{2,20}$/i, //姓名
+		isNumber: /^[1-9][0-9]*$/i, //数字
+		email: /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/i, //邮箱
+		money: /^([1-9][\d]{0,7}|0)(\.[\d]{1,2})?$/i, //金钱
 	}
-}
-var reg = new HandlerReg().isVerify("code","112")
-console.log(reg);
+	var repformatting = {
+		// 银行卡号 每四位 一个空格
+		SpaceBankCode: function (val) {
+			return (val + "").replace(/\s/g, '').replace(/(.{4})/g, "$1 ")
+		},
+		//银行卡号 显示四三位和后三位
+		encryptBankCode: function (val) {
+			return (val + "").replace(/^(.{4}).*(.{3})$/, "$1 **** **** *$2")
+		},
+		//手机号码 显示前三位和后三位
+		phone:function(val){
+			return (val + "").replace(/^(.{3}).*(.{3})$/, "$1*****$2")
+		},
+		// 更改姓名显示，只显示最后一个
+		userName: function (val) {
+			var strArr = val.split(""),
+				len = strArr.length - 1,
+				i = 0;
+			for (; i < len; i++) {
+				strArr[i] = strArr[i].replace(strArr[i], "*");
+			}
+			return strArr.join("");
+		},
+	}
+	//验证方法
+	function isVerify(type, val) {
+		return resType[type].test(val);
+	}
+	//替换方法
+	function replaceType(type, val) {
+		return repformatting[type](val);
+	}
+	//暴露方法对象
+	return {
+		isVerify: isVerify,
+		replaceType: replaceType
+	}
+};
+console.log(HandlerReg().replaceType('encryptBankCode', '6228480078809886479'));
